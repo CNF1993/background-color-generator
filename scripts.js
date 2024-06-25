af276138b2589d97a27919ff9ce151b7d23aac4b
CFN1993 <137438729+CFN1993@users.noreply.github.com>
2024-06-25 17:51:39 -0300

    change scripts

--- scripts.js
+++ scripts.js
@@ -3,23 +3,8 @@ const hexCode = document.querySelector("#hex-code");
 const button = document.querySelector("#button");
 const hexLetter = ["A", "B", "C", "D", "E", "F"];
 button.onclick = changeBackgroundColor;
-/*
-al clickear cambiar fondo
-    obtener codigo al clickear
-    reescribir #hexCode
-    cambiar fondo con el codigo obtenido
 
-Generar codigo hex
-    repetir 6 veces 
-        agregar letra o numero aleatorio
 
-Generar letra aleatoria
-    buscar en array hexLetter indice aleatorio
-
-Generar nro aleatorio entero del 0 al 9
-
-*/
-console.log(generateHexCode())
 
 function changeBackgroundColor(){
     let finalHexCode = generateHexCode();
@@ -36,17 +21,13 @@ function generateHexCode(){
             hexCode += generateLetter();
         }
     }
-    
     return hexCode;
 }
 
 function generateNumber () {
-    return Math.floor(Math.random() * 11)
+    return Math.floor(Math.random() * 10)
 }
 
 function generateLetter(){
     return hexLetter[Math.floor(Math.random() * hexLetter.length)]
 }
-
-
-
